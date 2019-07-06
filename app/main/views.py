27051da21fa2257fd969ad63e2b83fe241209f9a
Tony Kihara <tonykiharatonkin@gmail.com>
2019-07-06 23:47:01 +0300

    added a function for updating profile picture.

--- app/main/views.py
+++ app/main/views.py
@@ -33,4 +33,13 @@ def update_profile(uname):
         db.session.commit()
         return redirect(url_for('.profile',uname = user.username))
     return render_template('profile/update.html',form = form)
+@main.route('/user/<uname>/update/pic', methods = ['POST'])
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile', uname = uname))
 
